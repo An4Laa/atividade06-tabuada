821a6c158236a1ac02446404797cfc4f31bba9fd
An4Laa <analaurarochadesouza6@gmail.com>
2023-10-11 11:33:21 -0300

    Atividade tabuada

---
+++ numerotabuada.py
@@ -0,0 +1,14 @@
+numero = int(input("Digite um número inteiro: "))
+tabuada0 = numero * 0 
+tabuada1 = numero * 1
+tabuada2 = numero * 2
+tabuada3 = numero * 3
+tabuada4 = numero * 4
+tabuada5 = numero * 5
+tabuada6 = numero * 6
+tabuada7 = numero * 7
+tabuada8 = numero * 8
+tabuada9 = numero * 9
+tabuada10 = numero * 10
+tabuada_completa = {tabuada0, tabuada1, tabuada2, tabuada3, tabuada4, tabuada5, tabuada6, tabuada7, tabuada8, tabuada9, tabuada10}
+print("A tabuada do seu número é:", tabuada_completa)
